cb0619ee30fe840f4998635c9501b0c63d14a4d3
Дмитрий <dmitrijvencikov242@gmail.com>
2020-10-16 20:05:52 +0300

    удалил пробелы

--- src/test/java/ThreePackSeleniumTest/ThreeTest.java
+++ src/test/java/ThreePackSeleniumTest/ThreeTest.java
@@ -7,18 +7,12 @@ public class ThreeTest extends  BaseClass{
     @Test
     public void TestFun()
     {
-
-
        // getDriver().get("https://www.google.ru/");
         getDriver().get("https://www.google.com/search?q=%D0%B2%D0%BA&oq=%D0%B2%D0%BA&aqs=chrome.0.69i59l2j0l6.2179j0j15&sourceid=chrome&ie=UTF-8");
-
         getDriver().manage().window().maximize();
         InitePage in = new InitePage(getDriver());
-
-
         in.setSearchText("Привет");
         in.clickSubmitSearch();
-
     }
 
 }
